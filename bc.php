a29f9e5358122d431eaafae4aecedccbd1ec7086
philip_windows10 <348180212@qq.com>
2019-12-16 18:05:20 +0800

    原来是语法错误

--- bc.php
+++ bc.php
@@ -1,44 +1,41 @@
 <?php
 session_start();
-// var_dump($_SESSION);
 $session["message"]="请输入正确的单号";
 $con=mysqli_connect("localhost","root","guotong","it_think_sql");
-if($con){
-  // echo "连接成功";
-}
-else{
-  echo "连接失败". die(mysqli_connect_error());
-}
+
 $product_name=isset($_POST['product_name'])? $_POST["product_name"]:null; $product_name=mysqli_real_escape_string($con,$product_name); 
 $query="SELECT id FROM think_product WHERE product_name={$product_name}";
 if($sql=mysqli_query($con,$query))
 {
-  $productMath=mysqli_num_rows($sql);
-  if($productMath){
-  $query="INSERT IGNORE INTO `think_image_path` ( `product_image`) VALUES ($product_name)";
-  // $image_sql=mysqli_query($con,$query);
-  // echo $image_sql;
-  // if($image_sql){
-  //   echo "更新成功";
-  // }else{
-  //   return "此货物已经过, 扫过重复的单号";
+  $query2="SELECT * FROM think_image_path WHERE 'product_image'='$product_name'";
+  $result=mysqli_query($con,$query2);
+  var_dump($result);
+  $numbers=mysqli_num_rows($result);
+  echo $numbers; 
+  // if($numbers>0){
+  // while($row=mysqli_fetch_array($result)){
+  //   $db_name=$row['product_image'];
   // }
-
-  // $sql=mysqli_query($con,$query);
+  // if($product_name==$db_name){
+  //   echo $product_name."已经录入过了";
+  // }
+  // else{
+  // $query="INSERT IGNORE INTO `think_image_path` ( `product_image`) VALUES ($product_name)";
   $_SESSION['message']="可以上架";
   echo $_SESSION['message'];
   session_unset();
-  }else{
-
+  // }
+  // }
+}else{
   $_SESSION['message']="输入的商品信息有误";
   echo $_SESSION['message'];
   session_unset();
   }
-}else{
-  $_SESSION['message']="请输入正确的产品";
-  echo $_SESSION['message'];
-}
-
+// }
+// else{
+//   $_SESSION['message']="请输入正确的产品";
+//   echo $_SESSION['message'];
+// }
 
 mysqli_close($con);
 ?>
