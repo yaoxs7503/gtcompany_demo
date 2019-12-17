abe247e17755dd6fe1ecc7218954bca530bcc4d2
philip_windows10 <348180212@qq.com>
2019-12-17 14:35:02 +0800

    "标准化的文件进程" 查询的标准用法

--- readfile.php
+++ readfile.php
@@ -8,10 +8,19 @@ while (($filename = readdir($handler)) !== false)
             $files[] = $filename ;  
        }  
    }  
-   var_dump($files);
+//    var_dump($files);
 closedir($handler);
 foreach ($files as $value) {  
     echo $value;
 }    
 
+
+// 数据库连接
+$con=mysqli_connect("localhost","root","guotong","it_think_sql");
+$file_number=count($files);
+var_dump($file_number);
+
+$query2="SELECT * FROM think_image_path LIMIT 1,{$file_number}";
+$picture_query=mysqli_query($con,$query2);
+var_dump($picture_query);
 ?>
