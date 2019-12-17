a376742dd268144486f2424d97833f1f41b2dd2f
philip_windows10 <348180212@qq.com>
2019-12-17 15:04:21 +0800

    第一版的图片转换程序

--- readfile.php
+++ readfile.php
@@ -14,10 +14,13 @@ foreach ($files as $value) {
     echo $value;
 }    
 
-
 // 数据库连接
 $con=mysqli_connect("localhost","root","guotong","it_think_sql");
+if(count($files)>0){
 $file_number=count($files);
+}else{
+ echo "暂时没有文件";
+}
 var_dump($file_number);
 
 $query2="SELECT * FROM think_image_path LIMIT 1,{$file_number}";
@@ -26,7 +29,16 @@ $picture_query=mysqli_query($con,$query2);
 while($row=mysqli_fetch_array($picture_query)){
     // var_dump($row);
     echo $row['product_image'];
-    $picture[]=$row['product_image'];
+    $pictures[]=$row['product_image'];
 }
-var_dump($picture);
+// var_dump($pictures);
+if($file_number>0){
+    for ($i=0; $i <$file_number ; $i++) { 
+        echo $files[$i];
+        rename("picture/{$files[$i]}","images/{$pictures[$i]}.jpg");
+    }
+
+}else{
+    echo "暂时没有相片可以进行存储";
+}
 ?>
