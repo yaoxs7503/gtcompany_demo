06a3e42454afb5bf87ac52c6cf17083dee330aad
philip_windows10 <348180212@qq.com>
2019-12-17 12:04:00 +0800

    图片重命名的标准版本

---
+++ readfile.php
@@ -0,0 +1,17 @@
+<?php
+$dir="picture";
+$handler=opendir($dir);
+while (($filename = readdir($handler)) !== false) 
+{
+    if ($filename !== "." && $filename !== "..") 
+    {  
+            $files[] = $filename ;  
+       }  
+   }  
+   var_dump($files);
+closedir($handler);
+foreach ($files as $value) {  
+    echo $value, PHP_EOL;  
+}    
+
+?>
