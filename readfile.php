6917ec0e531ecde1f7a044e4b3c2d4ca15221118
philip_windows10 <348180212@qq.com>
2019-12-18 11:23:23 +0800

    删除图片确定最终版本

--- readfile.php
+++ readfile.php
@@ -3,7 +3,7 @@ $dir = "picture";
 $handler = opendir($dir);
 $files = [];
 $pictures = [];
-$files_temp = [];
+// $files_temp = [];
 while (($filename = readdir($handler)) !== false) {
     if ($filename !== "." && $filename !== "..") {
         $files[] = $filename;
@@ -12,21 +12,25 @@ while (($filename = readdir($handler)) !== false) {
 closedir($handler);
 
 $con = mysqli_connect("localhost", "root", "guotong", "it_think_sql");
-// if (count($files) > 0) {
+
+if (count($files) > 0) {
     $file_number = count($files);
-    $query2 = "SELECT * FROM think_image_path   ORDER BY add_time ASC LIMIT 0,{$file_number} ";
+    $query2 = "SELECT * FROM think_image_path WHERE flag=0 ORDER BY add_time ASC LIMIT 0,{$file_number} ";
     $picture_query = mysqli_query($con, $query2);
+    $picture_numbers = mysqli_num_rows($picture_query);
+    if ($picture_numbers>0){
     while ($row = mysqli_fetch_array($picture_query)) {
         $pictures[] = $row['product_image'];
     }
     $picturesNumber = count($pictures);
     for ($i = 0; $i < $picturesNumber; $i++) {
             rename("picture/{$files[$i]}", "images/{$pictures[$i]}.jpg");
-            array_push($files_temp, $pictures[$i]);
-        var_dump($pictures);
-        var_dump($files_temp);
-        
+    $query3="UPDATE think_image_path SET flag=1, image_name='{$pictures[$i]}.jpg' where product_image={$pictures[$i]}";
+    $picture_update=mysqli_query($con,$query3);
     }
-// } else {
-//     echo "暂时没有文件";
-// }
+}else{
+    echo "已经扫描过相关文件";
+}
+} else {
+    echo "暂时没有文件";
+}
